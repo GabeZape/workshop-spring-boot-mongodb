eb8582b4b20d2f202f63823d7c5415862d590a66
GabeZape <gabrielzapellaro@gmail.com>
2023-10-09 08:27:54 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/gabezape/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.gabezape.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabezape.workshopmongo.domain.User;
+import com.gabezape.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
